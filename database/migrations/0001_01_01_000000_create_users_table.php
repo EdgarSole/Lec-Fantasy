f047dd63a634b53cde6291d919ae94ec111966d5
Edgar <edgarhardwell@hotmail.com>
2025-05-27 14:26:42 +0200

    Base de datos actualizada

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('foto_url')->nullable(); 
+            $table->string('foto_url')->nullable();
+            $table->text('descripcion')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
